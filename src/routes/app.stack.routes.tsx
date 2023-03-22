c93add61c1dfd2171d97aeecf68a51b448165a0b
Vitor <paiva.v@icloud.com>
2023-03-22 12:46:38 -0300

    feat :sparkles:(routes): create app route

---
+++ src/routes/app.stack.routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '@screens/Home';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppStackRoutes() {
+  return (
+    <Navigator>
+      <Screen name='Home' component={Home} />
+    </Navigator>
+  );
+}
