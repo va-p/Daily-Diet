320032e1027ff034790c800a53ff5ff97f9dc616
Vitor <paiva.v@icloud.com>
2023-03-22 13:50:35 -0300

    style :nail_care:(app theme): create app theme and configure styled components provider

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+import { ThemeProvider } from 'styled-components';
+
 import { Routes } from '@routes/index';
+
+import { THEME } from 'src/global/styles/theme';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={THEME}>
       <StatusBar barStyle='light-content' />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,22 @@
+export const THEME = {
+  COLORS: {
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    GREY_1: '#1B1D1E',
+    GREY_2: '#333638',
+    GREY_3: '#5C6265',
+    GREY_4: '#B9BBBC',
+    GREY_5: '#DDDEDF',
+    GREY_6: '#EFF0F0',
+    GREY_7: '#FAFAFA',
+
+    WHITE: '#FFFFFF',
+  },
+  FONTS: {},
+};
