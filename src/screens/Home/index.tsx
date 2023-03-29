4d624c47a62cbcf4833f27749ab05e58a21902f6
Vitor <paiva.v@icloud.com>
2023-03-29 12:59:11 -0300

    feat :sparkles:(home): creates home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,62 @@
 import React from 'react';
-import { Container } from './styles';
+import { SectionList } from 'react-native';
+import {
+  Container,
+  Header,
+  ContainerProfileImage,
+  ProfileImage,
+  MealsContainer,
+  Title,
+  SectionHeader,
+} from './styles';
+
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { Plus } from 'phosphor-react-native';
+
+import { MealListItem } from '@components/MealListItem';
+import { Percent } from '@components/Percent';
+import { Button } from '@components/Button';
+
+import Logo from '@assets/Logo.svg';
+
+import { THEME } from '@themes/theme';
+
+import { MEALS } from '../../data/meals';
 
 export function Home() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <Logo width={82} height={37} />
+
+        <ContainerProfileImage>
+          <ProfileImage source={{ uri: '@assets/VaP.png' }} />
+        </ContainerProfileImage>
+      </Header>
+
+      <Percent type='primary' />
+
+      <Title>Refeições</Title>
+      <Button
+        type='primary'
+        icon={<Plus size={18} color={THEME.COLORS.WHITE} />}
+        title='Nova Refeição'
+      />
+
+      <MealsContainer>
+        <SectionList
+          sections={MEALS}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <MealListItem data={item} />}
+          renderSectionHeader={({ section }) => (
+            <SectionHeader>{section.title}</SectionHeader>
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: getBottomSpace(),
+          }}
+        />
+      </MealsContainer>
+    </Container>
+  );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,47 @@
+import { THEME } from '@themes/theme';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 24px 0 32px;
+`;
+
+export const ContainerProfileImage = styled.View`
+  min-width: 40px;
+  max-width: 40px;
+  min-height: 40px;
+  max-height: 40px;
+  border: 2px solid ${({ theme }) => theme.COLORS.GRAY_200};
+  border-radius: 20px;
+`;
+
+export const ProfileImage = styled.Image`
+  width: 100%;
+  height: 100px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.REGULAR};
+  font-size: ${RFValue(16)}px;
+  padding-top: 40px;
+  padding-bottom: 8px;
+`;
+
+export const MealsContainer = styled.View`
+  flex: 1
+  padding-top: 40px;
+`;
+
+export const SectionHeader = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.BOLD};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => THEME.COLORS.GRAY_100};
+`;
