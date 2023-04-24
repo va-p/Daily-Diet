e1ba773534b26f803cde14f7c7ac55111c867b1d
Vitor <paiva.v@icloud.com>
2023-04-24 18:39:07 -0300

    feat :sparkles:(routes): adds screens for navigation

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { RegisterMeal } from '@screens/RegisterMeal';
+import { Statistics } from '@screens/Statistics';
 import { Home } from '@screens/Home';
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -9,11 +11,16 @@ const { Navigator, Screen } = createNativeStackNavigator();
 export function AppStackRoutes() {
   return (
     <Navigator
+      initialRouteName='Home'
       screenOptions={{
         headerShown: false,
       }}
     >
       <Screen name='Home' component={Home} />
+
+      <Screen name='Estatísticas' component={Statistics} />
+
+      <Screen name='Registrar Refeição' component={RegisterMeal} />
     </Navigator>
   );
 }
